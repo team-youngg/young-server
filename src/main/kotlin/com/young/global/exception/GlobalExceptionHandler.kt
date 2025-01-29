b66e8a93217915062328a5cba6c0882158a93c8c
su080915 <tbr335@naver.com>
2025-01-29 22:45:27 +0900

    fix: error global handler

--- src/main/kotlin/com/young/global/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/young/global/exception/GlobalExceptionHandler.kt
@@ -1,11 +1,23 @@
 package com.young.global.exception
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
-    @ExceptionHandler(Exception::class)
+    @ExceptionHandler(CustomException::class)
     fun handleException(e: CustomException) = ErrorResponse.of(e)
+
+    @ExceptionHandler(Exception::class)
+    fun handleGenericException(e: Exception): ResponseEntity<ErrorResponse> {
+        val errorResponse = ErrorResponse(
+            message = "Unexpected error occurred: ${e.message}",
+            status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            code = e.javaClass.name
+        )
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse)
+    }
 }
