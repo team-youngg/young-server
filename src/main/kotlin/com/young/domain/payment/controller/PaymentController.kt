a83e4baca6c581d1bcf9ce76ba034ec66d0d90fa
su080915 <tbr335@naver.com>
2025-02-01 21:56:31 +0900

    fix: confirm payment post

--- src/main/kotlin/com/young/domain/payment/controller/PaymentController.kt
+++ src/main/kotlin/com/young/domain/payment/controller/PaymentController.kt
@@ -2,14 +2,14 @@ package com.young.domain.payment.controller
 
 import com.young.domain.payment.dto.request.PayRequest
 import com.young.domain.payment.service.PaymentService
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class PaymentController (
     private val paymentService: PaymentService
 ) {
-    @RequestMapping("/payment/confirm")
+    @PostMapping("/payment/confirm")
     fun confirmPayment(@RequestBody request: PayRequest) = paymentService.confirmPayment(request)
 }
