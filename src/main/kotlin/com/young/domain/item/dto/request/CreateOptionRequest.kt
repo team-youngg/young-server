06d4786970f023df991c3b46b3dd60e35a80f466
su080915 <tbr335@naver.com>
2025-02-03 18:21:02 +0900

    fix: item color

--- src/main/kotlin/com/young/domain/item/dto/request/CreateOptionRequest.kt
+++ src/main/kotlin/com/young/domain/item/dto/request/CreateOptionRequest.kt
@@ -1,9 +1,9 @@
 package com.young.domain.item.dto.request
 
-import com.young.domain.item.domain.enums.ItemColor
 import com.young.domain.item.domain.enums.ItemSize
 
 data class CreateOptionRequest(
     val size: ItemSize,
-    val color: ItemColor
+    val color: String,
+    val hex: String
 )
