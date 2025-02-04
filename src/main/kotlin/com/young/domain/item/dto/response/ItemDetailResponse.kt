dc3e8364bd5bfa6ca9a4aaf3e5c3aeb16ea149e0
su080915 <tbr335@naver.com>
2025-02-04 09:04:22 +0900

    feat: item detail response

---
+++ src/main/kotlin/com/young/domain/item/dto/response/ItemDetailResponse.kt
@@ -0,0 +1,32 @@
+package com.young.domain.item.dto.response
+
+import com.young.domain.item.domain.entity.Item
+import com.young.domain.item.domain.entity.ItemOption
+import com.young.domain.item.domain.entity.ItemOptionValue
+
+data class ItemDetailResponse(
+    val id: Long,
+    val name: String,
+    val description: String,
+    val detail: String,
+    val price: Long,
+    val options: List<OptionStockResponse>,
+    val images: List<String>,
+) {
+    companion object {
+        fun of(item: Item, images: List<String>, options: List<ItemOption>, optionValues: List<ItemOptionValue>): ItemDetailResponse {
+            return ItemDetailResponse(
+                id = item.id!!,
+                name = item.name,
+                description = item.description,
+                detail = item.detail,
+                price = item.price,
+                options = options.map { itemOption ->
+                    OptionStockResponse.of(optionValues
+                        .filter { it.itemOption.id == itemOption.id })
+                },
+                images = images,
+            )
+        }
+    }
+}
