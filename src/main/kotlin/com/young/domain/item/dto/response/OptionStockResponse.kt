902a3b56c8988cb92c63f26cfb61452338bac3a6
su080915 <tbr335@naver.com>
2025-02-04 15:08:04 +0900

    feat: option id response

--- src/main/kotlin/com/young/domain/item/dto/response/OptionStockResponse.kt
+++ src/main/kotlin/com/young/domain/item/dto/response/OptionStockResponse.kt
@@ -3,11 +3,13 @@ package com.young.domain.item.dto.response
 import com.young.domain.item.domain.entity.ItemOptionValue
 
 data class OptionStockResponse(
+    val id: Long,
     val option: OptionResponse,
     val stock: Long
 ) {
     companion object {
         fun of(optionValues: List<ItemOptionValue>) = OptionStockResponse(
+            id = optionValues.firstOrNull()?.itemOption?.id ?: 0,
             option = OptionResponse.of(optionValues),
             stock = optionValues.firstOrNull()?.itemOption?.stock ?: 0
         )
