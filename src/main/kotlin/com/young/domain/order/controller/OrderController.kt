14c4e9613a71e4f4ee557fe2c30e6eaaef8f3655
su080915 <tbr335@naver.com>
2025-02-05 20:14:34 +0900

    comment: remove todo

--- src/main/kotlin/com/young/domain/order/controller/OrderController.kt
+++ src/main/kotlin/com/young/domain/order/controller/OrderController.kt
@@ -15,9 +15,6 @@ import org.springframework.web.bind.annotation.RestController
 class OrderController (
     private val orderService: OrderService
 ) {
-    // TODO (웹) 결제 승인하기 버튼 눌르면 오더 요청보내기
-    // TODO (서버) 받을 요청은... 유저랑,
-
     @Operation(summary = "주문", description = "상품을 주문합니다.")
     @PostMapping
     fun orderFromCart(@RequestBody requests: OrderManyRequest) = orderService.order(requests)
