bd965a1e03c6c4abef3e099e86f795e79dce1aa3
su080915 <tbr335@naver.com>
2025-04-19 18:01:33 +0900

    feat: inquiry api

--- src/main/kotlin/com/young/domain/post/controller/InquiryController.kt
+++ src/main/kotlin/com/young/domain/post/controller/InquiryController.kt
@@ -1,11 +1,44 @@
 package com.young.domain.post.controller
 
+import com.young.domain.post.dto.request.CreateInquiryCommentRequest
+import com.young.domain.post.dto.request.CreateInquiryRequest
+import com.young.domain.post.dto.request.UpdateInquiryCommentRequest
+import com.young.domain.post.dto.request.UpdateInquiryRequest
 import com.young.domain.post.service.InquiryService
-import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.*
 
-@Controller
-class InquiryController (
+@RestController
+@RequestMapping("/inquiries")
+class InquiryController(
     private val inquiryService: InquiryService
 ) {
 
+    @PostMapping
+    fun createInquiry(@RequestBody request: CreateInquiryRequest) = inquiryService.createInquiry(request)
+
+    @GetMapping
+    fun getAllInquiries() = inquiryService.getAllInquiries()
+
+    @GetMapping("/{inquiryId}")
+    fun getInquiry(@PathVariable inquiryId: Long) = inquiryService.getInquiry(inquiryId)
+
+    @PutMapping("/{inquiryId}")
+    fun updateInquiry(@PathVariable inquiryId: Long, @RequestBody request: UpdateInquiryRequest)
+    = inquiryService.updateInquiry(inquiryId, request)
+
+    @DeleteMapping("/{inquiryId}")
+    fun deleteInquiry(@PathVariable inquiryId: Long) = inquiryService.deleteInquiry(inquiryId)
+
+    @PostMapping("/comments")
+    fun createComment(@RequestBody request: CreateInquiryCommentRequest) = inquiryService.createComment(request)
+
+    @GetMapping("/comments/{inquiryId}")
+    fun getComments(@PathVariable inquiryId: Long) = inquiryService.getCommentsByInquiry(inquiryId)
+
+    @PutMapping("/comments/{commentId}")
+    fun updateComment(@PathVariable commentId: Long, @RequestBody request: UpdateInquiryCommentRequest)
+    = inquiryService.updateComment(commentId, request)
+
+    @DeleteMapping("/comments/{commentId}")
+    fun deleteComment(@PathVariable commentId: Long) = inquiryService.deleteComment(commentId)
 }
