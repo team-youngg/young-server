c56588268c6e0088a064b06f0557cb64d36573e4
su080915 <tbr335@naver.com>
2025-04-19 18:15:24 +0900

    feat: add swagger api docs descriptions

--- src/main/kotlin/com/young/domain/post/controller/InquiryController.kt
+++ src/main/kotlin/com/young/domain/post/controller/InquiryController.kt
@@ -5,40 +5,51 @@ import com.young.domain.post.dto.request.CreateInquiryRequest
 import com.young.domain.post.dto.request.UpdateInquiryCommentRequest
 import com.young.domain.post.dto.request.UpdateInquiryRequest
 import com.young.domain.post.service.InquiryService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
 
+@Tag(name = "문의", description = "문의 api")
 @RestController
 @RequestMapping("/inquiries")
 class InquiryController(
     private val inquiryService: InquiryService
 ) {
-
+    @Operation(summary = "문의 작성", description = "문의를 작성합니다.")
     @PostMapping
     fun createInquiry(@RequestBody request: CreateInquiryRequest) = inquiryService.createInquiry(request)
 
+    @Operation(summary = "문의 전체 조회", description = "전체 문의를 조회합니다.")
     @GetMapping
     fun getAllInquiries() = inquiryService.getAllInquiries()
 
+    @Operation(summary = "문의 조회", description = "문의를 조회합니다.")
     @GetMapping("/{inquiryId}")
     fun getInquiry(@PathVariable inquiryId: Long) = inquiryService.getInquiry(inquiryId)
 
+    @Operation(summary = "문의 수정", description = "문의를 수정합니다.")
     @PutMapping("/{inquiryId}")
     fun updateInquiry(@PathVariable inquiryId: Long, @RequestBody request: UpdateInquiryRequest)
     = inquiryService.updateInquiry(inquiryId, request)
 
+    @Operation(summary = "문의 삭제", description = "문의를 삭제합니다.")
     @DeleteMapping("/{inquiryId}")
     fun deleteInquiry(@PathVariable inquiryId: Long) = inquiryService.deleteInquiry(inquiryId)
 
+    @Operation(summary = "문의 답변 작성", description = "문의 답변을 작성합니다.")
     @PostMapping("/comments")
     fun createComment(@RequestBody request: CreateInquiryCommentRequest) = inquiryService.createComment(request)
 
+    @Operation(summary = "문의 답변 전체 조회", description = "문의 답변을 전체 조회합니다.")
     @GetMapping("/comments/{inquiryId}")
     fun getComments(@PathVariable inquiryId: Long) = inquiryService.getCommentsByInquiry(inquiryId)
 
+    @Operation(summary = "문의 답변 수정", description = "문의 답변을 수정합니다.")
     @PutMapping("/comments/{commentId}")
     fun updateComment(@PathVariable commentId: Long, @RequestBody request: UpdateInquiryCommentRequest)
     = inquiryService.updateComment(commentId, request)
 
+    @Operation(summary = "문의 답변 삭제", description = "문의 답변을 삭제합니다.")
     @DeleteMapping("/comments/{commentId}")
     fun deleteComment(@PathVariable commentId: Long) = inquiryService.deleteComment(commentId)
 }
--- src/main/kotlin/com/young/domain/post/controller/NoticeController.kt
+++ src/main/kotlin/com/young/domain/post/controller/NoticeController.kt
@@ -3,26 +3,34 @@ package com.young.domain.post.controller
 import com.young.domain.post.dto.request.CreateNoticeRequest
 import com.young.domain.post.dto.request.UpdateNoticeRequest
 import com.young.domain.post.service.NoticeService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
 
+@Tag(name = "공지", description = "공지 api")
 @RestController
 @RequestMapping("/notices")
 class NoticeController(
     private val noticeService: NoticeService
 ) {
+    @Operation(summary = "공지 작성", description = "공지를 작성합니다.")
     @PostMapping
     fun createNotice(@RequestBody request: CreateNoticeRequest) = noticeService.createNotice(request)
 
+    @Operation(summary = "공지 전체 조회", description = "전체 공지를 조회합니다.")
     @GetMapping
     fun getAllNotices() = noticeService.getAllNotices()
 
+    @Operation(summary = "공지 조회", description = "공지를 조회합니다.")
     @GetMapping("/{noticeId}")
     fun getNotice(@PathVariable noticeId: Long) = noticeService.getNotice(noticeId)
 
+    @Operation(summary = "공지 수정", description = "공지를 수정합니다.")
     @PutMapping("/{noticeId}")
     fun updateNotice(@PathVariable noticeId: Long, @RequestBody request: UpdateNoticeRequest)
     = noticeService.updateNotice(noticeId, request)
 
+    @Operation(summary = "공지 삭제", description = "공지를 삭제합니다.")
     @DeleteMapping("/{noticeId}")
     fun deleteNotice(@PathVariable noticeId: Long) = noticeService.deleteNotice(noticeId)
 }
